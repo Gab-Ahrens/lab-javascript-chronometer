ef651b3bcb5836e0c67c52562cea7115abe8f4fe
Gabriel Ahrens <gab.ahrens@gmail.com>
2022-03-30 12:13:37 +0200

    lab completo

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -1,34 +1,42 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    (this.currentTime = 0), (this.intervalId = null);
   }
 
   start(callback) {
-    // ... your code goes here
+    this.intervalId = setInterval(() => {
+      this.currentTime += 1;}
+    , 1000);
   }
 
   getMinutes() {
-    // ... your code goes here
+    let minutesCount = Math.floor(this.currentTime / 60);
+    return minutesCount;
   }
 
   getSeconds() {
-    // ... your code goes here
+    let secondsCount = Math.floor(this.currentTime % 60);
+    return secondsCount;
   }
 
   computeTwoDigitNumber(value) {
-    // ... your code goes here
+    let twoDigits = value.toString();
+    if (twoDigits.length < 2) {
+      twoDigits = "0" + value.toString();
+    }
+    return twoDigits;
   }
 
   stop() {
-    // ... your code goes here
+    clearInterval(this.intervalId);
   }
 
   reset() {
-    // ... your code goes here
+    this.currentTime = 0;
   }
 
   split() {
-    // ... your code goes here
+    return `${this.computeTwoDigitNumber(this.getMinutes())}:${this.computeTwoDigitNumber(this.getSeconds())}`
   }
 }
 
